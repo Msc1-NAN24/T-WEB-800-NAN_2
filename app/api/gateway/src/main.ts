9f4e6abf18336db6f0f5d0da493ab940322a4074
eliemoriceau <elie.moriceau@epitech.eu>
2023-02-21 16:33:55 +0100

    fix: docker for api

--- app/api/gateway/src/main.ts
+++ app/api/gateway/src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  console.log(`server listen port 3000`);
+  console.log(`Hello`);
 }
 bootstrap();
