6ac3e1b155d210dc787f7bd3d6a7073768d3f574
MDeghaud67 <michel.deghaud@gmail.com>
2023-03-06 12:56:37 +0100

    login avatar

--- app/frontend/src/components/LoginAvatar/LoginAvatar.tsx
+++ app/frontend/src/components/LoginAvatar/LoginAvatar.tsx
@@ -3,5 +3,19 @@ export type LoginAvatarProps = {
 }
 
 export default function LoginAvatar(props: LoginAvatarProps) {
-  return (<h1>Login avatar</h1>)
+  return (
+    props.user == undefined ? (
+      <>
+        <h1>Login avatar</h1>
+        <button className="login-buttons"></button>
+      </>
+    ) : (
+      <>
+        <h1>Login avatar</h1>
+        <p className="user-avatar" title="Mathéo Bellanger">Matthéo Bellanger</p>
+        <button className="login-buttons" hidden></button>
+      </>
+    )
+    
+  )
 }
