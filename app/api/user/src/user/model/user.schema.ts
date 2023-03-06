37fdab9bdcc59aea182613461a5a8bbd8b5dbf4f
eliemoriceau <62339484+eliemoriceau@users.noreply.github.com>
2023-03-06 15:39:45 +0100

    Feat/user crud (#13)
    * feat: add User CRUD and test
    
    * fix: User test and refactor test
    
    * Update ci.yaml
    
    * fix: test ci api
    
    * Update ci.yaml
    
    * feat: add cmd test:ci
    
    * feat: add cmd test:ci
    
    * fix: typo cmd test:ci
    
    * Update ci.yaml
    
    * fix: script test:ci in all api
    
    * fix: add pnpm-lock.yaml
    
    * Update ci.yaml

---
+++ app/api/user/src/user/model/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ trim: true, unique: false })
+  lastName: string;
+  @Prop({ trim: true, unique: false })
+  firstName: string;
+  @Prop({ trim: true, unique: true })
+  email: string;
+  @Prop({ select: false })
+  password: string;
+  @Prop({ trim: true, index: true, unique: true, sparse: true })
+  phone: string;
+  @Prop({ isRequired: false })
+  picture?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
