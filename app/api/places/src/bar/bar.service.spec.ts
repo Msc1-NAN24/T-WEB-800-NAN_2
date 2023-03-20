01818b05ad1dcb89fe2396ff33a1705f365633f3
nico <nicolas.sansd@gmail.com>
2023-03-20 17:49:03 +0100

    fixed places TU

--- app/api/places/src/bar/bar.service.spec.ts
+++ app/api/places/src/bar/bar.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BarService } from './bar.service';
+import {BarController} from "./bar.controller";
 
 describe('BarService', () => {
   let service: BarService;
@@ -7,6 +8,7 @@ describe('BarService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [BarService],
+      controllers: [BarController],
     }).compile();
 
     service = module.get<BarService>(BarService);
