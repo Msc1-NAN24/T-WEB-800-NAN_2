94d9bc2db61855b7b14206a018109d48f261bc72
MDeghaud67 <michel.deghaud@gmail.com>
2023-03-20 17:17:16 +0100

    event card

--- app/frontend/src/components/Events/EventCard/EventCard.cy.tsx
+++ app/frontend/src/components/Events/EventCard/EventCard.cy.tsx
@@ -1,1 +1,14 @@
-export {}
+import EventCard from "./EventCard"
+//import cy from 'cypress'
+
+describe('<EventCard />', () => {
+    it('renders', () => {
+        cy.mount(<EventCard title={""} date={""} lieu={""} description={""} />)
+
+        //cy.get(img)
+
+        cy.get('h3')
+        cy.get('p')
+        cy.get('p')
+    })
+})
--- app/frontend/src/components/Events/EventCard/EventCard.tsx
+++ app/frontend/src/components/Events/EventCard/EventCard.tsx
@@ -1,3 +1,17 @@
-export default function EventCard() {
-  return <div>EventCard</div>
+interface EventsProps {
+  title: string,
+  date: string,
+  lieu: string,
+  description: string
+}
+
+export default function EventCard(props: EventsProps) {
+  return (
+    <>
+      <h1>EventCard</h1>
+      <h3>{props.title}</h3>
+      <p>{props.date} - {props.lieu}</p>
+      <p>{props.description}</p>
+    </>
+  );
 }
