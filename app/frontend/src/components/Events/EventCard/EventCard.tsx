7c9f04ca485908b47047c537bc84fd751be605f8
MDeghaud67 <michel.deghaud@gmail.com>
2023-03-21 11:56:48 +0100

    image

--- app/frontend/src/components/Events/EventCard/EventCard.tsx
+++ app/frontend/src/components/Events/EventCard/EventCard.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import { Card } from "react-bootstrap";
-
+import imgCard from "../../../../public/dddepth-127.jpg";
+import Image from "next/image";
 interface EventsProps {
   title: string,
   date: string,
@@ -16,6 +18,7 @@ export default function EventCard(props: EventsProps) {
         {/*<h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.title}</h3>
         <p className="font-normal text-gray-700 dark:text-gray-400">{props.date} - {props.lieu}</p>
         <p className="font-normal text-gray-700 dark:text-gray-400">{props.description}</p>*/}
+        <Image src={imgCard} alt="" />
         <h3>Atelier maison: créer ses patisseries</h3>
         <p>21 janvier à 16h - Nantes, Commerce</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
