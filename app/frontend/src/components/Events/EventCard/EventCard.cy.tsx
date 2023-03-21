d84749d52edd71466964508f11542c74b5d7810d
MDeghaud67 <michel.deghaud@gmail.com>
2023-03-21 16:30:37 +0100

    event card

--- app/frontend/src/components/Events/EventCard/EventCard.cy.tsx
+++ app/frontend/src/components/Events/EventCard/EventCard.cy.tsx
@@ -5,7 +5,7 @@ describe('<EventCard />', () => {
     it('renders', () => {
         cy.mount(<EventCard title={""} date={""} lieu={""} description={""} />)
 
-        //cy.get(img)
+        cy.get('img')
 
         cy.get('h3')
         cy.get('p')
